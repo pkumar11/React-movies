2090699a499ab4b718b69b2f671fe107e9c5bc9c
studentpankaj <pankaj10101993@gmail.com>
2019-10-10 15:16:04 +0530

    movie app

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => (
+	<div className="rmdb-header">
+		<div className="rmdb-header-content">
+			<Link to="/">
+				<img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+			</Link>
+		</div>
+	</div>
+);
+
+export default Header;
